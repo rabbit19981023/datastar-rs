16622643d629ee30f62a0d877b08ec47d4cec8ef
Yu Chen <rabbit19981023@gmail.com>
2026-01-13 01:39:28 +0800

    example: axum + maud example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use maud::{Markup, html};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!(
+        "server is listening on {}...",
+        listener.local_addr().unwrap()
+    );
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello() -> Markup {
+    html! {
+        h1 { "Hello, world!" }
+    }
 }
